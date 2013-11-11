69e05685d27391762edfaafae5df3caae31a27c3
rholdy <rholdy@gmail.com>
2013-11-11 15:26:12 -0700

    Assets precompile False....

--- config/application.rb
+++ config/application.rb
@@ -61,7 +61,7 @@ module Moji
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-
+    config.assets.compile = false
     
   end
 end
