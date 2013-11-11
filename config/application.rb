86a100137b005d8cffa45a84f248c1547d00ab80
rholdy <rholdy@gmail.com>
2013-11-11 15:23:11 -0700

    Assets precompile False...

--- config/application.rb
+++ config/application.rb
@@ -61,9 +61,7 @@ module Moji
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.paths << Emoji.images_path
 
-    config.assets.precompile << "emoji/*.png"
     
   end
 end
